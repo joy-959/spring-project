c9e2b13477f7ab944aacd1f0e78a0774ab8657ac
a60157285 <ulimee959@gmail.com>
2024-07-12 14:45:59 +0900

    first commit 스프링 2.7.7 ver 세팅 h2데이터베이스 연결 테이블 생성

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner demo(PersonRepository repository){
+		return (args) -> {
+			repository.save(new Person("John Doe"));
+			repository.save(new Person("John Doe"));
+		};
+	}
+}
---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    // 기본 생성자
+    public Person() {
+    }
+
+    // 매개변수를 받는 생성자
+    public Person(String name) {
+        this.name = name;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
